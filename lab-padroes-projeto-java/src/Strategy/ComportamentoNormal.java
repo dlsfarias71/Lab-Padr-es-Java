79f299377b26a3001155c48fa60031542e91281f
Luciana de Sá <102324477+dlsfarias71@users.noreply.github.com>
2022-05-29 22:10:36 -0300

    Add files via upload

---
+++ lab-padroes-projeto-java/src/Strategy/ComportamentoNormal.java
@@ -0,0 +1,13 @@
+package Strategy;
+
+public class ComportamentoNormal implements Comportamento{
+
+	@Override
+	public void mover() {
+	System.out.println("Movendo-se normalmente...");
+		
+	}
+	
+	
+
+}
--- lab-padroes-projeto-java/src/one/digitalinnovation/gof/Test.java
+++ lab-padroes-projeto-java/src/one/digitalinnovation/gof/Test.java
@@ -1,8 +1,15 @@
 package one.digitalinnovation.gof;
 
+import Strategy.ComportamentoAgressivo;
+import Strategy.ComportamentoDefensivo;
+import Strategy.ComportamentoNormal;
+import Strategy.Robo;
+
 public class Test {
 	
-	public static void main(String[]args) {
+	//Singleton
+	
+	public static <Comportamento> void main(String[]args) {
 		SingletonLazy lazy = SingletonLazy.getIntancia();
 		System.out.println(lazy);
 		lazy =SingletonLazy.getIntancia();
@@ -17,6 +24,30 @@ public class Test {
 		System.out.println(lazyHolder);
 		lazyHolder =SingletonLazyHolder.getIntancia();
 		System.out.println(lazyHolder);
+		
+		//Strategy
+		
+      ComportamentoNormal normal = new ComportamentoNormal();
+      ComportamentoDefensivo defensivo = new ComportamentoDefensivo();
+      ComportamentoAgressivo agressivo = new  ComportamentoAgressivo();
+		
+		
+  
+		
+		
+		Robo robo = new Robo();
+		robo.setComportamento(normal);
+		robo.mover();
+		robo.mover();
+		robo  = new Robo();
+		robo.setComportamento(defensivo);
+		robo.mover();
+	    robo = new Robo();
+		robo.setComportamento(agressivo);
+		robo.mover();
+		robo.mover();
+		robo.mover();
+		
 	}
 
 }
